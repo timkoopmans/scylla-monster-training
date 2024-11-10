13bf514305e59e4e3c03d1a9b6278dc5e8d0893e
timkoopmans <tim.koops@gmail.com>
2024-11-10 11:01:48 +1100

    add challenge 6 hot partitions

--- src/config.rs
+++ src/config.rs
@@ -5,7 +5,12 @@ pub struct Challenge {
     pub id: String,
     pub description: String,
     pub setup: String,
+    #[serde(default = "default_solve")]
     pub solve: Vec<String>,
+}
+
+fn default_solve() -> Vec<String> {
+    Vec::new()
 }
 
 #[derive(Debug, Deserialize)]
